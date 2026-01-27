54cff89f24facf2f8619ee749406c0f14418fabd
Hubert Bojda <hubert.bojda12@gmail.com>
2026-01-27 20:41:32 +0100

    Add .gitignore and uncomment VM configuration in cloud-init.tf and variables.tf for clarity

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,19 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.2-rc01"
     }
+  }
+  backend "local" {
+    path = "./terraform.tfstate"
   }
 }
 
 provider "proxmox" {
-  pm_api_url = var.proxmox_api_url
-  pm_api_token_id = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token
-  pm_tls_insecure = true
+  pm_api_url                  = var.proxmox_api_url
+  pm_api_token_id             = var.proxmox_api_token_id
+  pm_api_token_secret         = var.proxmox_api_token
+  pm_tls_insecure             = true
   pm_minimum_permission_check = false
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,42 +1,42 @@
-variable proxmox_api_url {
+variable "proxmox_api_url" {
   type = string
 }
 
-variable proxmox_api_token_id {
+variable "proxmox_api_token_id" {
   type = string
 }
 
-variable proxmox_api_token {
+variable "proxmox_api_token" {
   type = string
 }
 
-  variable "vm_configs" {
-    type = map(object({
-      vm_id       = number
-      target_node = string
-      name        = string
-      memory      = number
-      vm_state    = string
-      onboot      = bool
-      startup     = string
-      ipconfig    = string
-      cores       = number
-      bridge      = string
-      network_tag = number
-    }))
-    default = {
-      "master-node1" = {
-        vm_id       = 3000
-        target_node = "pve1"
-        name        = "test"
-        memory      = 4096
-        vm_state    = "stopped"
-        onboot      = false
-        startup     = "order=1"
-        ipconfig    = "ip=192.168.4.100/24,gw=192.168.4.1"
-        cores       = 2
-        bridge      = "vmbr0"
-        network_tag = 0
-      },
-    }
+variable "vm_configs" {
+  type = map(object({
+    vm_id       = number
+    target_node = string
+    name        = string
+    memory      = number
+    vm_state    = string
+    onboot      = bool
+    startup     = string
+    ipconfig    = string
+    cores       = number
+    bridge      = string
+    network_tag = number
+  }))
+  default = {
+    "master-node1" = {
+      vm_id       = 3000
+      target_node = "pve1"
+      name        = "test"
+      memory      = 4096
+      vm_state    = "stopped"
+      onboot      = false
+      startup     = "order=1"
+      ipconfig    = "ip=192.168.4.100/24,gw=192.168.4.1"
+      cores       = 2
+      bridge      = "vmbr0"
+      network_tag = 0
+    },
   }
+}
