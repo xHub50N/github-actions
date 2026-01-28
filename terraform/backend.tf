ba4d26b05712dfb61d5f0610e6cb451707105e86
Hubert Bojda <hubert.bojda12@gmail.com>
2026-01-28 20:14:59 +0100

    Update Terraform backend configuration to use S3 with specified settings

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,16 @@
 terraform {
-  backend "azurerm" {}
+  backend "s3" {
+    bucket = "terraform"
+    endpoints = {
+      s3 = "https://s3.hbojda.ovh"
+    }
+    key = "terraform.tfstate"
+
+    region                      = "main"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+  }
 }
