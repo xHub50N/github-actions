8232491df5905170dce86f15e587009d793560fd
Hubert Bojda <hubert.bojda12@gmail.com>
2026-02-02 20:54:36 +0100

    Add k3s-master and k3s-worker VM configurations to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,5 +41,31 @@ variable "vm_configs" {
       bridge      = "vmbr0"
       network_tag = 0
     },
+    "k3s-master" = {
+      vm_id       = 4000
+      target_node = "pve1"
+      name        = "k3s-master"
+      memory      = 4096
+      vm_state    = "stopped"
+      onboot      = false
+      startup     = "order=1"
+      ipconfig    = "ip=192.168.4.101/24,gw=192.168.4.1"
+      cores       = 2
+      bridge      = "vmbr0"
+      network_tag = 0
+    },
+    "k3s-worker" = {
+      vm_id       = 4001
+      target_node = "pve1"
+      name        = "k3s-worker"
+      memory      = 4096
+      vm_state    = "stopped"
+      onboot      = false
+      startup     = "order=1"
+      ipconfig    = "ip=192.168.4.102/24,gw=192.168.4.1"
+      cores       = 2
+      bridge      = "vmbr0"
+      network_tag = 0
+    },
   }
 }
